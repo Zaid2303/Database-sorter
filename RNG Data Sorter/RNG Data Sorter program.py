37e4ca314030a42b68f502531febf289a3744d5c
Zaid2303 <104207168+Zaid2303@users.noreply.github.com>
2023-11-22 11:54:11 +0000

    Added timer comparison working
    added timer and comparison count - adding graph creation, working but error with downloading library

--- RNG Data Sorter/RNG Data Sorter program.py
+++ RNG Data Sorter/RNG Data Sorter program.py
@@ -1,37 +1,41 @@
 import random
+import time
+import matplotlib.pyplot as plt
 
 # Function to generate random arrays
 def generate_random_array(size):
     return [random.randint(100000, 999999) for _ in range(size)]
 
-# Create arrays 
-HundredList = generate_random_array(100) # 100 paces
-ThousandList = generate_random_array(1000) # 1000 spaces
-TenThousandList = generate_random_array(10000) # 10000 spaces
-
 # Sorting algorithms
+
+# Selection Sort Algorithm
 def selection_sort(arr):
+    comparisons = 0
     n = len(arr)
     for i in range(n):
         min_index = i
         for j in range(i + 1, n):
+            comparisons += 1
             if arr[j] < arr[min_index]:
                 min_index = j
         arr[i], arr[min_index] = arr[min_index], arr[i]
+    return comparisons
 
-#merge sort 
+# Merge Sort Algorithm
 def merge_sort(arr):
+    comparisons = 0
     if len(arr) > 1:
         mid = len(arr) // 2
         left_half = arr[:mid]
         right_half = arr[mid:]
 
-        merge_sort(left_half)
-        merge_sort(right_half)
+        comparisons += merge_sort(left_half)
+        comparisons += merge_sort(right_half)
 
         i = j = k = 0
 
         while i < len(left_half) and j < len(right_half):
+            comparisons += 1
             if left_half[i] < right_half[j]:
                 arr[k] = left_half[i]
                 i += 1
@@ -50,65 +54,144 @@ def merge_sort(arr):
             j += 1
             k += 1
 
-# quick sort
+    return comparisons
+
+# Quick Sort Algorithm
 def quick_sort(arr):
+    comparisons = 0
     if len(arr) <= 1:
-        return arr
+        return comparisons
     pivot = arr[len(arr) // 2]
     left = [x for x in arr if x < pivot]
     middle = [x for x in arr if x == pivot]
     right = [x for x in arr if x > pivot]
-    return quick_sort(left) + middle + quick_sort(right)
+    comparisons += len(arr) - len(left) - len(right)
+    comparisons += quick_sort(left)
+    comparisons += quick_sort(right)
+    return comparisons
 
+# function that calculates time and comparison amount
+def measure_performance(algorithm, array):
+    start_time = time.time()
+    comparisons = algorithm(array)
+    end_time = time.time()
+    execution_time = (end_time - start_time) * 1000  # Convert to milliseconds
+    return execution_time, comparisons
 
+# Create arrays
+HundredList = generate_random_array(100)
+ThousandList = generate_random_array(1000)
+TenThousandList = generate_random_array(10000)
 
-# creates copy of Original list with .copy for selection sort and sorts it.
+# Measure and analyze performance for each sorting algorithm and dataset size
 HundredList_selection = HundredList.copy()
 ThousandList_selection = ThousandList.copy()
 TenThousandList_selection = TenThousandList.copy()
 
-selection_sort(HundredList_selection)
-selection_sort(ThousandList_selection)
-selection_sort(TenThousandList_selection)
-
-# creates copy of Original list with .copy for merge sort and sorts it.
 HundredList_merge = HundredList.copy()
 ThousandList_merge = ThousandList.copy()
 TenThousandList_merge = TenThousandList.copy()
 
-merge_sort(HundredList_merge)
-merge_sort(ThousandList_merge)
-merge_sort(TenThousandList_merge)
-
-# creates copy of Original list with .copy for quick sort and sorts it.
 HundredList_quick = HundredList.copy()
 ThousandList_quick = ThousandList.copy()
 TenThousandList_quick = TenThousandList.copy()
 
-quick_sort(HundredList_quick)
-quick_sort(ThousandList_quick)
-quick_sort(TenThousandList_quick)
+# Selection Sort
+time_selection, comparisons_selection = measure_performance(selection_sort, HundredList_selection)
+print(f"Selection Sort (HundredList): Execution Time: {time_selection} ms, Comparisons: {comparisons_selection}")
 
-# Debug use
-print (len(HundredList))
-print (len(ThousandList))
-print (len(TenThousandList))
+time_selection, comparisons_selection = measure_performance(selection_sort, ThousandList_selection)
+print(f"Selection Sort (ThousandList): Execution Time: {time_selection} ms, Comparisons: {comparisons_selection}")
 
-print("Selection Sort:")
-print(HundredList_selection[:10])  # Print the first 10 elements of the sorted array for verification
-print(ThousandList_selection[:10])
-print(TenThousandList_selection[:10])
+time_selection, comparisons_selection = measure_performance(selection_sort, TenThousandList_selection)
+print(f"Selection Sort (TenThousandList): Execution Time: {time_selection} ms, Comparisons: {comparisons_selection}")
 
-print("\nMerge Sort:")
-print(HundredList_merge[:10])
-print(ThousandList_merge[:10])
-print(TenThousandList_merge[:10])
+print ("")
 
-print("\nQuick Sort:")
-print(HundredList_quick[:10])
-print(ThousandList_quick[:10])
-print(TenThousandList_quick[:10])
+# Merge Sort
+time_merge, comparisons_merge = measure_performance(merge_sort, HundredList_merge)
+print(f"Merge Sort (HundredList): Execution Time: {time_merge} ms, Comparisons: {comparisons_merge}")
+
+time_merge, comparisons_merge = measure_performance(merge_sort, ThousandList_merge)
+print(f"Merge Sort (ThousandList): Execution Time: {time_merge} ms, Comparisons: {comparisons_merge}")
+
+time_merge, comparisons_merge = measure_performance(merge_sort, TenThousandList_merge)
+print(f"Merge Sort (TenThousandList): Execution Time: {time_merge} ms, Comparisons: {comparisons_merge}")
+
+print ("")
+
+# Quick Sort
+time_quick, comparisons_quick = measure_performance(quick_sort, HundredList_quick)
+print(f"Quick Sort (HundredList): Execution Time: {time_quick} ms, Comparisons: {comparisons_quick}")
+
+time_quick, comparisons_quick = measure_performance(quick_sort, ThousandList_quick)
+print(f"Quick Sort (ThousandList): Execution Time: {time_quick} ms, Comparisons: {comparisons_quick}")
+
+time_quick, comparisons_quick = measure_performance(quick_sort, TenThousandList_quick)
+print(f"Quick Sort (TenThousandList): Execution Time: {time_quick} ms, Comparisons: {comparisons_quick}")
 
 
+# Function to create line charts
+def plot_performance(data_sizes, execution_times, comparisons, algorithm_name):
+    plt.figure(figsize=(10, 5))
+
+    # Plot execution time
+    plt.subplot(1, 2, 1)
+    plt.plot(data_sizes, execution_times, marker='o', label='Execution Time')
+    plt.title(f'{algorithm_name} Performance')
+    plt.xlabel('Array Size')
+    plt.ylabel('Execution Time (ms)')
+    plt.legend()
+
+    # Plot comparisons
+    plt.subplot(1, 2, 2)
+    plt.plot(data_sizes, comparisons, marker='o', label='Comparisons')
+    plt.title(f'{algorithm_name} Comparisons')
+    plt.xlabel('Array Size')
+    plt.ylabel('Number of Comparisons')
+    plt.legend()
+
+    plt.tight_layout()
+    plt.show()
+
+# Function to run and plot sorting algorithm
+def run_and_plot_sorting_algorithm(algorithm, array, size):
+    array_copy = array.copy()
+    time, comparisons = measure_performance(algorithm, array_copy)
+    return time, comparisons
+
+# Create arrays
+data_sizes = [100, 1000, 10000]
+
+# Initialize lists to store results for each algorithm
+selection_execution_times = []
+selection_comparisons = []
+merge_execution_times = []
+merge_comparisons = []
+quick_execution_times = []
+quick_comparisons = []
+
+# Run and plot Selection Sort
+for size in data_sizes:
+    time_selection, comparisons_selection = run_and_plot_sorting_algorithm(selection_sort, generate_random_array(size), size)
+    selection_execution_times.append(time_selection)
+    selection_comparisons.append(comparisons_selection)
+
+# Run and plot Merge Sort
+for size in data_sizes:
+    time_merge, comparisons_merge = run_and_plot_sorting_algorithm(merge_sort, generate_random_array(size), size)
+    merge_execution_times.append(time_merge)
+    merge_comparisons.append(comparisons_merge)
+
+# Run and plot Quick Sort
+for size in data_sizes:
+    time_quick, comparisons_quick = run_and_plot_sorting_algorithm(quick_sort, generate_random_array(size), size)
+    quick_execution_times.append(time_quick)
+    quick_comparisons.append(comparisons_quick)
+
+# Plot graphs for each algorithm
+plot_performance(data_sizes, selection_execution_times, selection_comparisons, 'Selection Sort')
+plot_performance(data_sizes, merge_execution_times, merge_comparisons, 'Merge Sort')
+plot_performance(data_sizes, quick_execution_times, quick_comparisons, 'Quick Sort')
 
 
